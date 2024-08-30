211f46626e018d555e88e5bda0bf2c650cfe6abf
Sivtheng <sivtheng25@gmail.com>
2024-08-30 15:22:45 +0700

    colors

--- src/components/stuff.svelte
+++ src/components/stuff.svelte
@@ -8,28 +8,28 @@
   <ul class="list-disc pl-5">
     <li class="mb-4">
       <a href="https://github.com/Sivtheng/about-sivtheng" class="text-[#EEEEEE] border-b border-accent hover:text-accent hover:border-b-accent" target="_blank" rel="noopener noreferrer">this site</a>
-      <div class="nested-info">> built with svelte and tailwindcss.</div>
+      <div class="nested-info">> built with <span style="color: #f73c00;">svelte</span> and <span style="color: #36b7f0;">tailwindcss</span>.</div>
       <div class="nested-info">> deployed on vercel (free version).</div>
     </li>
     <li class="mb-4">
       <a href="  https://github.com/SokvisalMong/pawgadoptioncenter" class="text-[#EEEEEE] border-b border-accent hover:text-accent hover:border-b-accent" target="_blank" rel="noopener noreferrer">pawg-adoption-center</a>
-      <div class="nested-info">> webdev1 group project built with reactjs and deployed on vercel.</div>
+      <div class="nested-info">> webdev1 group project built with <span style="color: #5FD3F3;">reactjs</span> and deployed on vercel.</div>
       <div class="nested-info">> this is very rushed and bad. one day i will remake it.</div>
     </li>
     <li class="mb-4">
       <a href="https://github.com/Sivtheng/expense-tracker-maxy" class="text-[#EEEEEE] border-b border-accent hover:text-accent hover:border-b-accent" target="_blank" rel="noopener noreferrer">expense-tracker</a>
-      <div class="nested-info">> built with ruby-on-rails and sqlite3 database.</div>
+      <div class="nested-info">> built with <span style="color: #C60000;">ruby-on-rails</span> and <span style="color: #127BC4;">sqlite3</span>.</div>
       <div class="nested-info">> no money to host but you can run it locally by following the readme.</div>
     </li>
     <li class="mb-4">
       <a href="https://github.com/Sivtheng/distributed-system-caching" class="text-[#EEEEEE] border-b border-accent hover:text-accent hover:border-b-accent" target="_blank" rel="noopener noreferrer">redis-caching for distributed-system-class</a>
-      <div class="nested-info">> basic laravel crud that showcase caching with redis.</div>
+      <div class="nested-info">> basic <span style="color: #F82C1E;">laravel</span> crud that showcase caching with <span style="color: #D6362A;">redis</span>.</div>
       <div class="nested-info">> can't test, i forgot the commands.</div>
     </li>
     <li class="mb-4">
       <a href="https://github.com/Sivtheng/alexandria-bookbuying-subsystem" class="text-[#EEEEEE] border-b border-accent hover:text-accent hover:border-b-accent" target="_blank" rel="noopener noreferrer">bookbuying-subsystem for enterprise-application-development-class</a>
-      <div class="nested-info">> built with laravel</div>
-      <div class="nested-info">> can't test, it requires api from golang-server which is not live anymore.</div>
+      <div class="nested-info">> built with <span style="color: #F82C1E;">laravel</span></div>
+      <div class="nested-info">> can't test, it requires api from <span style="color: #76ABAE;">golang</span> server which is not live anymore.</div>
     </li>
     <li class="mb-4">
       <a href="https://github.com/Sivtheng/resume-parser" class="text-[#EEEEEE] border-b border-accent hover:text-accent hover:border-b-accent" target="_blank" rel="noopener noreferrer">resume-parser model for ai-class</a>
