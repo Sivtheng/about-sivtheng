b997642e633c8cb857e22f0d2fa15bbb23c42368
Sivtheng <sivtheng25@gmail.com>
2024-09-23 10:14:03 +0700

    23sep2024update

--- src/components/stuff.svelte
+++ src/components/stuff.svelte
@@ -4,7 +4,7 @@
 
 <div class="container mx-auto p-8 bg-[#31363F] text-[#EEEEEE] text-left mb-16">
   <h1 class="text-4xl mb-4 text-accent">personal projects</h1>
-  <p class="mb-4">some stuff i did in my free time/work when i have nothing better to do. click on the link to view on github</p>
+  <p class="mb-4">some stuff i did in my free time/work when i have nothing better to do. click on the link to view on github.</p>
   <ul class="list-disc pl-5">
     <li class="mb-4">
       <a href="https://github.com/Sivtheng/about-sivtheng" class="text-[#EEEEEE] border-b border-accent hover:text-accent hover:border-b-accent" target="_blank" rel="noopener noreferrer">this site</a>
@@ -12,13 +12,18 @@
       <div class="nested-info">> deployed on vercel (free version).</div>
     </li>
     <li class="mb-4">
+      <a href="https://github.com/Sivtheng/mean-mailservices" class="text-[#EEEEEE] border-b border-accent hover:text-accent hover:border-b-accent" target="_blank" rel="noopener noreferrer">mail services</a>
+      <div class="nested-info">> built with mean (<span style="color: #55AD48;">mongodb</span>, <span style="color: #F3E02A;">expressjs</span>, <span style="color: #C51336;">angularjs</span>, <span style="color: #86CF35;">nodejs</span>) with <span style="color: #31D377;">mailtrap</span> and <span style="color: #E3139B;">graphql</span> as a <a href="/structure.png" target="_blank" rel="noopener noreferrer" class="text-[#EEEEEE] border-b border-accent hover:text-accent hover:border-b-accent">monolithic structure</a>.</div>
+      <div class="nested-info">> the purpose of this project is practicing how to implement various mail services.</div>
+    </li>
+    <li class="mb-4">
       <a href="https://github.com/Sivtheng/todo-sivtheng" class="text-[#EEEEEE] border-b border-accent hover:text-accent hover:border-b-accent" target="_blank" rel="noopener noreferrer">simple todo-list app</a>
-      <div class="nested-info">> built with <span style="color: #3CBB86;">vue.js</span> and backend as a service with <span style="color: #FACD37;">firebase</span>.</div>
+      <div class="nested-info">> built with <span style="color: #3CBB86;">vuejs</span> and backend as a service with <span style="color: #FACD37;">firebase</span>.</div>
       <div class="nested-info">> deployed with firebase hosting (free version).</div>
     </li>
     <li class="mb-4">
       <div>pawg-adoption-center</div>
-      <div class="nested-info">> webapp for pet adoption site called "pawg-adoption-center"</div>
+      <div class="nested-info">> webapp for pet adoption site called "pawg-adoption-center".</div>
       <div class="nested-info">> <span><a href="https://github.com/Sivtheng/pawg-frontend" class="text-[#EEEEEE] border-b border-accent hover:text-accent hover:border-b-accent" target="_blank" rel="noopener noreferrer">frontend</a></span> with <span style="color: #75D5D0;">nextjs</span> and <span style="color: #36b7f0;">tailwindcss</span>.</div>
       <div class="nested-info">> <span><a href="https://github.com/Sivtheng/pawg-dashboard" class="text-[#EEEEEE] border-b border-accent hover:text-accent hover:border-b-accent" target="_blank" rel="noopener noreferrer">dashboard</a></span> with <span style="color: #3D79A9;">django</span> and <span style="color: #7210F5;">bootstraps</span>.</div>
       <div class="nested-info">> <span><a href="https://github.com/Sivtheng/pawg-backend" class="text-[#EEEEEE] border-b border-accent hover:text-accent hover:border-b-accent" target="_blank" rel="noopener noreferrer">backend</a></span> with <span style="color: #76ABAE;">golang</span> and <span style="color: #31648D;">postgresql</span>. </div>
@@ -35,13 +40,13 @@
     </li>
     <li class="mb-4">
       <a href="https://github.com/Sivtheng/alexandria-bookbuying-subsystem" class="text-[#EEEEEE] border-b border-accent hover:text-accent hover:border-b-accent" target="_blank" rel="noopener noreferrer">bookbuying-subsystem for enterprise-application-development-class</a>
-      <div class="nested-info">> built with <span style="color: #F82C1E;">laravel</span></div>
+      <div class="nested-info">> built with <span style="color: #F82C1E;">laravel</span>.</div>
       <div class="nested-info">> can't test, it requires api from <span style="color: #76ABAE;">golang</span> server which is not live anymore.</div>
     </li>
     <li class="mb-4">
       <a href="https://github.com/Sivtheng/resume-parser" class="text-[#EEEEEE] border-b border-accent hover:text-accent hover:border-b-accent" target="_blank" rel="noopener noreferrer">resume-parser model for ai-class</a>
-      <div class="nested-info">> a model that can predict the job category the resume belongs to with an accuracy evaluation of around 0.60 (lmao, it is abit better than just guessing)</div>
-      <div class="nested-info">> made this in 2 days to pass the class lol</div>
+      <div class="nested-info">> a model that can predict the job category the resume belongs to with an accuracy evaluation of around 0.60 (lmao, it is abit better than just guessing).</div>
+      <div class="nested-info">> made this in 2 days to pass the class lol.</div>
     </li>
   </ul>
 </div>
